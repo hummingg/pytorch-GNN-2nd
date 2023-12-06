714f07f2fe479af07937b860d144f75ba98ac5cf
hummingg <xuhuiming6991@gmail.com>
2023-12-06 09:53:36 +0800

    use BERTModel

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 
 本代码还有配套的资源包
 资源包需要通过www.aianaconda.com网站进行下载。
+
+
+默认同步 /tmp/pycharm_project_954
+我的设置 /tmp/pycharm_project_pytorch-GNN
+结果2个都存在，而且生成的文件在/tmp/pycharm_project_pytorch-GNN
--- code_15_BERT_PROPN.py
+++ code_15_BERT_PROPN.py
@@ -9,7 +9,7 @@ Created on Fri Apr 10 07:10:37 2020
 
 #提取代词特征
 
-import pandas as pd 
+import pandas as pd
 import pickle
 import torch
 from tqdm import tqdm
@@ -18,30 +18,30 @@ from transformers import BertTokenizer,BertModel,BertConfig
 #指定设备
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
-    
-#读取数据    
-df_test = pd.read_csv("gap-development.tsv", delimiter="\t")
+
+#读取数据
+df_test = pd.read_csv("gap-coreference/gap-development.tsv", delimiter="\t")
 df_train_val = pd.concat([
-    pd.read_csv("gap-test.tsv", delimiter="\t"),
-    pd.read_csv("gap-validation.tsv", delimiter="\t")
-], axis=0)    
+    pd.read_csv("gap-coreference/gap-test.tsv", delimiter="\t"),
+    pd.read_csv("gap-coreference/gap-validation.tsv", delimiter="\t")
+], axis=0)
 
 
 def getmodel():
     #加载词表文件tokenizer
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    
+    tokenizer = BertTokenizer.from_pretrained('huggingface/bert-base-uncased')
+
     #添加特殊词
     special_tokens_dict = {'additional_special_tokens': ["[THISISA]","[THISISB]","[THISISP]"]}
     tokenizer.add_special_tokens(special_tokens_dict)	#添加特殊词
     print(tokenizer.additional_special_tokens,tokenizer.additional_special_tokens_ids)
 
-    
-    model = BertModel.from_pretrained('bert-base-uncased')#加载模型
+
+    model = BertModel.from_pretrained('huggingface/bert-base-uncased')#加载模型
     return tokenizer,model
 
 
-    
+
 
 ############################
 
@@ -50,13 +50,13 @@ def insert_tag(row,hasbrack=True):#按照插入的位置，从大到小排序[(3
     orgtag=[" [THISISA] "," [THISISB] "," [THISISP] "]
     if hasbrack==False:
         orgtag=[" THISISA "," THISISB "," THISISP "]
-        
+
     to_be_inserted = sorted([
         (row["A-offset"], orgtag[0]),
         (row["B-offset"], orgtag[1]),
         (row["Pronoun-offset"], orgtag[2])], key=lambda x: x[0], reverse=True)
-    
-    text = row["Text"]#len 443 
+
+    text = row["Text"]#len 443
     for offset, tag in to_be_inserted:#先插最后的，不会影响前面
         text = text[:offset] + tag + text[offset:]#（插到每个代词的前面）
     return text#len 470 (443+3*9)
@@ -76,30 +76,35 @@ def tokenize(sequence_ind, tokenizer,sequence_mask= None):#将标签分离，并
         final_tokens.append(one)
         if sequence_mask is not None:
             final_mask.append(sequence_mask[i])
-    return  final_tokens, (entries["[THISISA]"], entries["[THISISB]"], entries["[THISISP]"]) ,final_mask   
+    return  final_tokens, (entries["[THISISA]"], entries["[THISISB]"], entries["[THISISP]"]) ,final_mask
 
 
 
 def savepkl(df,name):
-    bert_prediction = []    
-    for _, row in tqdm(df.iterrows(),total=len(df)):    
+    bert_prediction = []
+    for _, row in tqdm(df.iterrows(),total=len(df)):
         #循环内部
         text = insert_tag(row)#插入标签
         sequence_ind = tokenizer.encode(text)#向量化
-        tokens, offsets,_ = tokenize(sequence_ind, tokenizer)#获取标签偏移        
+        tokens, offsets,_ = tokenize(sequence_ind, tokenizer)#获取标签偏移
         token_tensor = torch.LongTensor([tokens]).to(device)
-        bert_outputs,bert_last_outputs=  model(token_tensor)  #[1, 107, 768] , [1, 768]            
+        # 错误写法
+        # bert_outputs,bert_last_outputs =  model(token_tensor)  #[1, 107, 768] , [1, 768]
+        # res = model(token_tensor)
+        # bert_outputs, bert_last_outputs = res['last_hidden_state'], res['pooler_output']
+        bert_outputs,bert_last_outputs =  model(token_tensor)[:2]  #[1, 107, 768] , [1, 768]
         extracted_outputs = bert_outputs[:,offsets,:]#根据偏移位置抽取特征向量
-        bert_prediction.append(extracted_outputs.cpu().numpy())    
+        bert_prediction.append(extracted_outputs.cpu().numpy())
     pickle.dump(bert_prediction, open(name, "wb"))
 
 
 if __name__ == '__main__':
-    
+
     tokenizer,model = getmodel()
     model.to(device)
     torch.set_grad_enabled(False)
-    
-    savepkl(df_test, 'test_bert_outputs_forPROPN.pkl')    
-    savepkl(df_train_val, 'bert_outputs_forPROPN.pkl')   
+
+    # 找不到文件？在/tmp/pycharm_project_pytorch-GNN
+    savepkl(df_test, 'test_bert_outputs_forPROPN.pkl')
+    savepkl(df_train_val, 'bert_outputs_forPROPN.pkl')
 
